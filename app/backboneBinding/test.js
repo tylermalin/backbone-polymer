27698aa700582e230840b32fc0dcf70218eb6862
Paolo Ardoino <paolo.ardoino@gmail.com>
2014-09-13 11:09:59 +0100

    Backbone.stickit / Polymer bindings

--- app/backboneBinding/test.js
+++ app/backboneBinding/test.js
@@ -4,12 +4,26 @@
 		type: ''
 	});
 
+	Backbone.Stickit.addHandler(_.extend(_.clone(_.find(Backbone.Stickit._handlers, { selector: 'input' }), true), { selector: 'paper-input' }));
+
+	Backbone.Stickit.addHandler({
+		selector: 'paper-dropdown',
+		events: ['core-select'],
+		update: function($el, val) {
+			console.log("HERE", val);
+			$el[0].selected = val; 
+		},
+		getVal: function($el) {
+			return $el[0].selected;
+		}
+	});
+
 	var MyView = Backbone.View.extend({
 
 		bindings: {
 			'#input-text': 'name',
-			'#input-text2': 'name',
 			'#paper-input-text': 'name',
+			'#input-paper-dropdown': 'type',
 			'#input-select': 'type'
 		},
 
@@ -17,8 +31,12 @@
 			this.template = _.template(
 				'<paper-input id="paper-input-text" label="Type something"></paper-input><br />' +
 				'<input type="text" id="input-text" /><br />'+
-				'<input type="text" id="input-text2" /><br />'+
-				'<select id="input-select"><option val="one">first</option><option value="two">second</option></select><br /><br><a id="link-1" href="javascript:void(0);">set name "PAOLO"</a> - <a id="link-2" href="javascript:void(0);">set type "second"</a><br><br><div id="log"></div>');
+				'<select id="input-select"><option value="one">first</option><option value="two">second</option></select><br />' +
+				'<paper-dropdown valueattr="val" id="input-paper-dropdown">' +
+				'<paper-item val="one" label="First"></paper-item>' + 
+				'<paper-item val="two" label="Second"></paper-item>' +
+				'</paper-dropdown>' +
+				'<br><a id="link-1" href="javascript:void(0);">set name "PAOLO"</a> - <a id="link-2" href="javascript:void(0);">set type "second"</a><br><br><div id="log"></div>'); 
 			this.model = new MyModel();
 			this.render();
 		},
@@ -49,7 +67,9 @@
 		}
 	});
 
-	var mv = new MyView({
-		el: $('body')
+	$(document.body).ready(function() {
+		var mv = new MyView({
+			el: $('body')
+		});
 	});
 })(jQuery);
